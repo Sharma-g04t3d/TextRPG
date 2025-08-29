12b5ef252b5c108c7bf615894227884475e373b4
Sharma <vikramaditya.toman@gmail.com>
2025-08-29 19:25:09 +0530

    inventory update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,63 +2,80 @@
 #include "samurai.h"
 #include "wizard.h"
 
-int main() {
-    //Initalizing all entities
-    entity e;
-    // Initializing players
-    mage m;
-    samurai w;
 
+int main() {
+    // Initializing players
+    wizard w;
+    samurai s;
     // Player inputs
     int atkname;
+    int turn = 0;
+    /*
+    w.inventory_add("HealSpell");
+    w.inventory_add("HealSpell");
+    w.inventory_add("HealSpell");
+    w.inventory_add("Wand");
+    w.inventory_add("Hat");
 
+    s.inventory_add("Potion");
+    s.inventory_add("Potion");
+    s.inventory_add("Potion");
+    s.inventory_add("Sword");
+    s.inventory_add("Armour");
+
+    std::cout<<"Inventory of : ["<<w.Name<<"]"<<std::endl;
+    for(auto items : w.inventory){
+        std::cout<<"Item_name : "<< items.first << std::endl;
+        std::cout<<"Item_slot_no : "<< items.second.first << std::endl;
+        std::cout<<"Item_count : "<< items.second.second << std::endl;
+    }
+    std::cout<<"Inventory of : ["<<s.Name<<"]"<<std::endl;
+    for(auto items : s.inventory){
+        std::cout<<"Item_name : "<< items.first << std::endl;
+        std::cout<<"Item_slot_no : "<< items.second.first << std::endl;
+        std::cout<<"Item_count : "<< items.second.second << std::endl;
+    }*/
+        
     // Main game loop
-    while (m.Health > 0 && w.Health > 0) {
-        switch (e.turn) {
+    while (w.Health > 0 && s.Health > 0) {
+        switch (turn) {
             case 0: {
                 std::cout << "\n[Warrior's Turn] Enter action (0 = Basic Attack, 1 = Special Attack): ";
                 std::cin >> atkname;
-
-                w.attack(atkname);
-                m.damage(w.atk);
-
+                s.attack(atkname);
+                w.damage(s.atk);
+                s.display();
                 w.display();
-                m.display();
-
-                e.turn = 1; // switch turn
+                turn = 1; // switch turn
+                s.turn += 1;
                 break;
             }
-
             case 1: {
-                std::cout << "\n[Mage's Turn] Enter action (0 = Attack, 1 = Heal): ";
+                std::cout << "\n[Wizard's Turn] Enter action (0 = Attack, 1 = Heal): ";
                 std::cin >> atkname;
-
-                if (atkname == 0) {
-                    m.attack(0);
-                    w.damage(m.atk);
+                if (atkname != 3) {
+                    w.attack(atkname);
+                    s.damage(w.atk);
                 } else {
-                    m.heal();
+                    w.heal();
                 }
-
+                s.display();
                 w.display();
-                m.display();
-
-                e.turn = 0; // switch turn
+                turn = 0; // switch turn
+                w.turn += 1;
                 break;
             }
         }
     }
-
     // Aftermath of battle
-    if (m.Health <= 0) {
-        std::cout << "\n[Mage Status] : Fainted\n";
-    } else if (w.Health <= 0) {
+    if (w.Health <= 0) {
+        std::cout << "\n[Wizard Status] : Fainted\n";
+    } else if (s.Health <= 0) {
         std::cout << "\n[Warrior Status] : Fainted\n";
     }
-    m.~mage();
-    w.~samurai();
+    w.~wizard();
+    s.~samurai();
     std::string EndQuote;
     std::cin >> EndQuote;
-
     std::cin.get(); // prevent console from closing immediately
 }
